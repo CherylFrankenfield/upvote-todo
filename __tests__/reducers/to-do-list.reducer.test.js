e94b1b9e53fe92aa70f0402610c68790580cc10f
Amy Churchwell and Cheryl Frankenfield <amy@>
2018-02-20 11:25:58 -0800

    Create passing tests for test and adding new item

--- __tests__/reducers/to-do-list.reducer.test.js
+++ __tests__/reducers/to-do-list.reducer.test.js
@@ -1,7 +1,37 @@
 import toDoListReducer from './../../src/reducers/to-do-list-reducer';
 
 describe('toDoListReducer', ()=> {
+
+  let action;
+  const sampleItemData = {
+    chore: 'Learn React',
+    description: 'Get EggHead subscription',
+    rating: 1,
+    id: 0
+  };
+
+
   test('Should return default state if no action type is recognized', ()=> {
     expect(toDoListReducer({}, {type: null})).toEqual({});
+  });
+
+
+  test('Should successfully add new item to mastertoDoList', () => {
+    const { chore, description, rating, id } = sampleItemData;
+    action = {
+      type: 'ADD_ITEM',
+      chore: chore,
+      description: description,
+      rating: rating,
+      id: id
+    };
+    expect(toDoListReducer({}, action)).toEqual({
+      [id] : {
+        chore: chore,
+        description: description,
+        rating: rating,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/to-do-list-reducer.js
+++ src/reducers/to-do-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+  case 'ADD_ITEM':
+    const { chore, description, rating, id } = action;
+    let newState = Object.assign({}, state, {
+      [id]: {
+        chore: chore,
+        description: description,
+        rating: rating,
+        id: id
+      }
+    });
+    return newState;
+  default:
+    return state;
+  }
 };
