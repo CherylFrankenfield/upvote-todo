4c74793cd215a7a5d20def8f15e159ba1fc41733
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-02-21 09:19:12 -0800

    import connect in Body component and renders now

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 function Body() {
   return (
@@ -8,4 +9,4 @@ function Body() {
   );
 }
 
-export default Body;
+export default connect()(Body);
