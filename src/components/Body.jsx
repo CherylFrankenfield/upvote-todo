da966a68d8b906e05560ad256957fbd6730e1e0a
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-02-21 09:49:55 -0800

    Add input field to Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 function Body() {
+  let _description = null;
+
   return (
     <div>
-      <p>Body works!</p>
+      <h1>My To-Do List</h1>
+      <label>Add an item:</label>
+      <input
+        type='text'
+        description='description'
+        ref={(input)=> {_description = input;}}/>
+      <button type='submit'>Add</button>
     </div>
   );
+}
+
+Body.propTypes = {
+
 }
 
 export default connect()(Body);
